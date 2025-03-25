945ed5194cd2593a42b6e51af1998d969e4e939e
a-bhula <ammaarbhula76@gmail.com>
2025-03-25 13:13:39 +0000

    Fix formatting in urls.py and update binary files in __pycache__

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,4 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
-]path("", include("blog.urls"), name="blog-urls"),
+]
